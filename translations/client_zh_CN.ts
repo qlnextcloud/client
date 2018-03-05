7b505562c9dc00c535a3e9e93bc267609457174a
heshushen <i.sshe@foxmail.com>
2018-03-05 15:39:38 +0800

    bug8725: 修改windows客户端链接向导页面文字描述2

--- translations/client_zh_CN.ts
+++ translations/client_zh_CN.ts
@@ -1288,7 +1288,7 @@ Continuing the sync as normal will cause all your files to be overwritten by an 
     <message>
         <location filename="../src/gui/creds/httpcredentialsgui.cpp" line="86"/>
         <source>&lt;a href=&quot;%1&quot;&gt;Click here&lt;/a&gt; to request an app password from the web interface.</source>
-        <translation>&lt;a href=&quot;%1&quot;&gt;点击这里&lt;/a&gt;从 web 界面请求一个 app 密码。</translation>
+        <translation>&lt;a href=&quot;%1&quot;&gt;点击这里&lt;/a&gt;从web界面请求一个密码。</translation>
     </message>
 </context>
 <context>
